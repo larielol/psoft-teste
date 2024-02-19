56bde9214734f8c8b941cf27db928bc28ec5f8ce
larielol <luccasariel010@hotmail.com.br>
2024-02-19 09:05:24 -0300

    teste

---
+++ teste/TestePsoft.java
@@ -0,0 +1,14 @@
+public class TestePsoft {
+
+    public static void main (String args[]) {
+
+        int num = 0;
+
+        for(int i = 0;i < 100; i++) {
+            num += 1;
+            System.out.println(num);
+        }
+
+    }
+    
+}
